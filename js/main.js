56ec2a16f3566c8e494480e71532356bc7c7cac6
Solomon19052015 <42998762+Solomon19052015@users.noreply.github.com>
2018-10-15 16:23:21 +0300

    files

--- js/main.js
+++ js/main.js
@@ -1,10 +1,21 @@
 window.onload = function(){
  
   $(".yy").classList.add('yy2');
-  $(".vv").classList.add('vv2');
+  
+  
+  
+
+  setTimeout(()=>{
+    $(".vv").classList.add('vv2');
+ },200)
+ 
   setTimeout(()=>{
     $(".cross").style.opacity = "1";
   },500)
+ 
+  setTimeout(()=>{
+    $(".a").classList.add("fadeTop");
+  },700)
  
 
   window.addEventListener("scroll", scrollHeadndler);
@@ -15,15 +26,13 @@ window.addEventListener("resize",function(){
 function scrollHeadndler(){
   let top =  window.pageYOffset;
   console.log(top);
- if(top > 50){
+ if(top > 100){
    $(".txt").classList.add("fadeIn");
  }
 
- if(top > 300){
+ if(top > 350){
    $(".aboutUs").classList.add("fadeIn");
  }
-
- 
 
 }
 
